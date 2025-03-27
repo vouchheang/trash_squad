49e447e159f0e2d920746aa732817604f3f85edc
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2025-03-27 14:05:35 +0700

    post register

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:trash_squad/components/form.dart';
+import 'package:trash_squad/controllers/user_controller.dart';
+import 'package:trash_squad/screens/login.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -12,6 +13,42 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   bool _agreeToTerms = false;
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final RegisterController _userController = RegisterController();
+
+  bool isValidEmail(String email) {
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
+  }
+
+  void _register() async {
+    final username = _usernameController.text;
+    final email = _emailController.text.trim();
+    final password = _passwordController.text;
+
+    if (!isValidEmail(email)) {
+      print("Invalid email format");
+      return;
+    }
+
+    final userRegister = await _userController.register(
+      username,
+      email,
+      password,
+      'user',
+    ); 
+    if (userRegister != null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginWidget()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Register failed'), backgroundColor: Colors.red),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,54 +64,108 @@ class _RegisterState extends State<Register> {
             fit: BoxFit.contain,
           ),
           const SizedBox(height: 20),
-          SizedBox(
-            width: 430,
-            child: Column(
-              children: const [
-                FormregisterWithShadow(
-                  hintText: "Location",
-                  iconPath: Icon(Icons.location_on),
+          Container(
+            margin: EdgeInsets.only(left: 25, right: 25),
+            child: TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(
+                  Icons.person_2_outlined,
+                  color: Color(0xFF086C74),
                 ),
-                SizedBox(height: 20),
-                FormregisterWithShadow(
-                  hintText: "Email",
-                  iconPath: Icon(Icons.email_outlined),
+                filled: true,
+                fillColor: const Color.fromARGB(255, 240, 240, 240),
+                contentPadding: const EdgeInsets.all(18),
+                hintText: "Username",
+                hintStyle: const TextStyle(
+                  color: const Color(0xFF5BB59B),
+                  fontSize: 20,
                 ),
-                SizedBox(height: 20),
-                FormregisterWithShadow(
-                  hintText: "Password",
-                  iconPath: Icon(Icons.lock_outlined),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
                 ),
-              ],
+              ),
             ),
           ),
+          SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.only(left: 25, right: 25),
+            child: TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.email_outlined,
+                  color: Color(0xFF086C74),
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(255, 240, 240, 240),
+                contentPadding: EdgeInsets.all(18),
+                hintText: "Email",
+                hintStyle: TextStyle(
+                  color: const Color(0xFF5BB59B),
+                  fontSize: 20,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.only(left: 25, right: 25),
+            child: TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.lock_outlined, color: Color(0xFF086C74)),
+                filled: true,
+                fillColor: const Color.fromARGB(255, 240, 240, 240),
+                contentPadding: EdgeInsets.all(18),
+                hintText: "Password",
+                hintStyle: TextStyle(
+                  color: const Color(0xFF5BB59B),
+                  fontSize: 20,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+
           const SizedBox(height: 50),
           Container(
-            margin: const EdgeInsets.symmetric(horizontal: 30),
+            margin: EdgeInsets.only(left: 30, right: 30),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
               boxShadow: [
                 BoxShadow(
-                  color: Colors.black.withValues(alpha: 0.2),
-                  blurRadius: 1,
+                  color: Color.fromARGB(255, 87, 87, 87),
+                  blurRadius: 40,
+                  spreadRadius: 0.0,
                 ),
               ],
             ),
             child: ElevatedButton(
-              onPressed: _agreeToTerms ? () {} : null, // Disable if unchecked
+              onPressed: _register,
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF5BB59B),
+                backgroundColor: Color(0xFF5BB59B),
                 foregroundColor: Colors.white,
-                minimumSize: const Size(380, 62),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ).copyWith(
-                foregroundColor: WidgetStateProperty.all(Colors.white),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                minimumSize: Size(465, 65),
               ),
-              child: const Text(
+              child: Text(
                 "Register",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20,
+                ),
               ),
             ),
           ),
@@ -91,36 +182,37 @@ class _RegisterState extends State<Register> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 GestureDetector(
-  onTap: () {
-    setState(() {
-      _agreeToTerms = !_agreeToTerms;
-    });
-  },
-  child: Container(
-    width: 17,  // Increased container size
-    height: 17, // Increased container size
-    decoration: BoxDecoration(
-      color: _agreeToTerms ? Colors.teal : Colors.transparent,
-      border: Border.all(color: Colors.white, width: 2),
-      borderRadius: BorderRadius.circular(4),
-    ),
-    child: _agreeToTerms
-      ? const Center(
-          child: Icon(
-            Icons.check,
-            size: 10,  
-            color: Colors.white,
-          ),
-        )
-      : null,
-  ),
-),
+                  onTap: () {
+                    setState(() {
+                      _agreeToTerms = !_agreeToTerms;
+                    });
+                  },
+                  child: Container(
+                    width: 17, // Increased container size
+                    height: 17, // Increased container size
+                    decoration: BoxDecoration(
+                      color: _agreeToTerms ? Colors.teal : Colors.transparent,
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child:
+                        _agreeToTerms
+                            ? const Center(
+                              child: Icon(
+                                Icons.check,
+                                size: 10,
+                                color: Colors.white,
+                              ),
+                            )
+                            : null,
+                  ),
+                ),
                 const SizedBox(width: 12),
                 // Terms text
                 Expanded(
                   child: RichText(
                     text: TextSpan(
-                      style: const TextStyle(color: Colors.white, fontSize: 12),
+                      style: const TextStyle(color: Colors.white, fontSize: 14),
                       children: [
                         const TextSpan(
                           text: 'By registering, you are agreeing with our ',
